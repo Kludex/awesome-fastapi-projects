4b4aa9a39106d1f916ced265e278658275ffaa19
Herivelton Andreassa <handreassa@gmail.com>
2023-03-09 09:13:41 -0300

    Removing venv folder from remote

--- .venv/lib/python3.9/site-packages/giturlparse/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import parse, rewrite  # NOQA
--- .venv/lib/python3.9/site-packages/typepy/error.py
+++
@@ -1,9 +0,0 @@
-"""
-.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
-"""
-
-
-class TypeConversionError(TypeError):
-    """
-    Exception raised when failed to convert data.
-    """
