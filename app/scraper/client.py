9fa3b8e43bebfdd43358be11580e135bc5ff9610
Vladyslav Fedoriuk <vlad.fedoriuk2000@gmail.com>
2023-07-31 23:16:37 +0200

    Add parsing of the SourceGraph SSE data

--- app/scraper/client.py
+++ app/scraper/client.py
@@ -1,23 +1,77 @@
 """The client for the SourceGraph API."""
+import datetime
 from collections.abc import AsyncGenerator, Mapping
 from contextlib import asynccontextmanager
 from datetime import timedelta
-from typing import Any, AnyStr, Final, Self
+from typing import Any, AnyStr, Final, Literal, NewType, Self
 from urllib.parse import quote
 
 import httpx
 import stamina
 from httpx_sse import EventSource, ServerSentEvent, aconnect_sse
-from pydantic import HttpUrl
+from pydantic import (
+    BaseModel,
+    Field,
+    HttpUrl,
+    NonNegativeInt,
+    TypeAdapter,
+    computed_field,
+)
 
 #: The URL of the SourceGraph SSE API.
 SOURCE_GRAPH_STREAM_API_URL: Final[
     HttpUrl
 ] = "https://sourcegraph.com/.api/search/stream"
 
+#: The ID of a repository from the SourceGraph API.
+SourceGraphRepoId = NewType("SourceGraphRepoId", int)
+
+
+class SourceGraphRepoData(BaseModel):
+    """The data of a repository."""
+
+    type: Literal["repo"]
+    repo_id: SourceGraphRepoId = Field(..., alias="repositoryID")
+    repo_handle: str = Field(..., alias="repository")
+    stars: NonNegativeInt = Field(..., alias="repoStars")
+    last_fetched_at: datetime.datetime = Field(..., alias="repoLastFetched")
+    description: str = Field(default="")
+
+    @computed_field
+    @property
+    def repo_url(self: Self) -> HttpUrl:
+        """The URL of the repository."""
+        return TypeAdapter(HttpUrl).validate_python(f"https://{self.repo_handle}")
+
+
+#: The type adapter for the SourceGraphRepoData.
+SourceGraphRepoDataAdapter = TypeAdapter(SourceGraphRepoData)
+#: The type adapter for the SourceGraphRepoData list.
+SourceGraphRepoDataListAdapter = TypeAdapter(list[SourceGraphRepoData])
+
+#: The query parameters for the SourceGraph SSE API.
+FASTAPI_REPOS_QUERY_PARAMS: Final[Mapping[str, str]] = {
+    "q": quote(
+        " ".join(
+            [
+                "repo:has.content(from fastapi import FastApi)",
+                "type:repo",
+                "visibility:public",
+                "archived:no",
+                "fork:no",
+            ]
+        )
+    ),
+}
+
 
 class AsyncSourceGraphSSEClient:
-    """A client for the SourceGraph SSE API."""
+    """
+    A client for the SourceGraph SSE API.
+
+    To learn more about the underlying API, see the ``SourceGraph SSE API``
+    https://docs.sourcegraph.com/api/stream_api#sourcegraph-stream-api
+    """
 
     def __init__(self: Self) -> None:
         """Initialize the client."""
@@ -68,18 +122,8 @@ class AsyncSourceGraphSSEClient:
 
     async def aiter_fastapi_repos(self: Self) -> AsyncGenerator[ServerSentEvent, None]:
         """Iterate over the SourceGraph SSE API with retries."""
-        params = {
-            "q": quote(
-                " ".join(
-                    [
-                        "repo:has.content(from fastapi import FastApi)",
-                        "type:repo",
-                        "visibility:public",
-                        "archived:no",
-                        "fork:no",
-                    ]
-                )
-            ),
-        }
-        async for event in self._aiter_sse_with_retries(params=params):
-            yield event
+        async for event in self._aiter_sse_with_retries(
+            params=FASTAPI_REPOS_QUERY_PARAMS
+        ):
+            if event.event == "matches":
+                yield SourceGraphRepoDataListAdapter.validate_python(event.json())
